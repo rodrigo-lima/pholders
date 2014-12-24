b0a7b9c7bb4949e4ffdbd997049532fe5319abed
Rodrigo Lima <rolima@Rodrigos-MacBook-Pro-2.local>
2014-12-23 23:36:20 -0600

    more stuff

--- pholders.rb
+++ pholders.rb
@@ -2,12 +2,11 @@
 
 require 'rubygems'
 require 'commander/import'
-
+require_relative 'debugUtils.rb'
 require_relative 'pholdersUtils.rb'
 
-
-# ---- 
-# some global vars here
+# -------------------------------------------------------------
+# global stuff here
 $user_home = Dir.home
 # OLD
 $old_simulators_home = $user_home + "/Library/Application Support/iPhone Simulator"
@@ -16,7 +15,7 @@ $old_simulators_apps = "/Applications"
 $new_simulators_home = $user_home + "/Library/Developer/CoreSimulator/Devices"
 $new_simulators_apps = "/data/Containers/Data/Application"
 
-
+# -------------------------------------------------------------
 program :version, '0.0.1'
 program :description, 'Finds iOS simulators folders and performs actions'
 
@@ -26,22 +25,36 @@ command :list do |c|
   c.syntax = 'pholders list [options]'
   c.description = 'List all simulators installations'
   c.option '--xcode [XCODE_TYPE]', [:xcode5, :xcode6, :both], 'Which Xcode simulators to find: xcode5, xcode6 or both?'
+  c.option '--includeEmpty', 'Includes simulators with no apps'
   c.action do |args, options|
     options.default :xcode => :both
 
-    say 'Listing Simulator Folders'
+    puts "------------"
+    DebugUtils.output_line "Listing Simulator Folders"
+    DebugUtils.output_line "  - #{options.xcode}"
+    DebugUtils.output_line "  - Including empty simulators" if options.includeEmpty
+    DebugUtils.output_line "  - Only simulator with apps" if not options.includeEmpty
+    puts "------------\n"
+
+    # OLD simulators
     if options.xcode == :xcode5 or options.xcode == :both
-      say "OLD : #{$old_simulators_home}"
-      puts PholdersUtils.list_sorted $old_simulators_home+"/*"      
-      say "OLD ==="
+      DebugUtils.output_line "OLD XCODE Simulators root path : #{$old_simulators_home}"
+      old_sims = PholdersUtils.old_simulators $old_simulators_home+"/*"
+      old_sims.each { |k,v|
+        DebugUtils.result_line "iPad/iPhone Simulator - #{v[:name]}"
+        DebugUtils.result_line "  Path - #{v[:path]}"
+      }
+      DebugUtils.output_line "===\n"
     end
+
+    # NEW simulators
     if options.xcode == :xcode6 or options.xcode == :both
-      say "NEW : #{$new_simulators_home}"
+      DebugUtils.output_line "NEW XCODE Simulators root path : #{$new_simulators_home}"
       new_sims = PholdersUtils.list_sorted $new_simulators_home+"/*"      
       new_sims.each { |e| 
         PholdersUtils.display_device_info e 
       }
-      say "NEW ==="
+      DebugUtils.output_line "===\n"
     end
   end
 end
--- pholdersUtils.rb
+++ pholdersUtils.rb
@@ -1,54 +1,65 @@
 #!/usr/bin/env ruby
 require 'cfpropertylist'
 
-#
 module PholdersUtils
     KRUNTIME_PREFIX = "com.apple.CoreSimulator.SimRuntime"
     KRUNTIME_PREFIX_LENGTH = KRUNTIME_PREFIX.length
 
     KCOM_APPLE_APPS_PREFIX = "com.apple."
-    KCOM_APPLE_APPS_PREFIX_LENGTH = KCOM_APPLE_APPS_PREFIX.length
-
+    KOLD_SIMULATORS_FOLDERS = ["User","Library"]
     KNEW_SIMULATORS_APPLICATIONSTATE_PLIST = "/data/Library/BackBoard/applicationState.plist"
 
     def PholdersUtils.list_sorted (folder)
-        # puts "... LISTING #{folder}"
         files = Dir.glob(folder)
-        # puts "1... #{files}"
+        #sort based on last modified
         files = files.map { |file| [File.new(file).mtime, file] }
-        # puts "2... #{files}"
         files = files.sort.map { |file| file[1] }
-        # puts "3... #{files}"
-        # puts "-------------"
-        # files.each { |file| puts file }
         files
     end
 
+    def PholdersUtils.old_simulators (folder)
+        all_sims = PholdersUtils.list_sorted folder
+        old_sims = {}
+        all_sims.each { |alls| 
+            last_path_component = alls.split('/').last
+            DebugUtils.debug_line "alls = #{alls} -- #{last_path_component}" if $verbose
+            if KOLD_SIMULATORS_FOLDERS.index(last_path_component) == nil 
+                old_sims[alls] = {
+                    :name => last_path_component,
+                    :path => alls
+                    }
+                DebugUtils.debug_line "old_sims[#{alls}] = #{old_sims[alls]}"  if $verbose
+            end
+        }
+        old_sims
+    end
+
     def PholdersUtils.display_device_info (device_folder)
-        puts "... DEVICE Folder: #{device_folder}"
         plist = CFPropertyList::List.new(:file => device_folder+"/device.plist")
         device = CFPropertyList.native_types(plist.value)
 
         # which simulator
         runtime = device['runtime']
         runtime = runtime[KRUNTIME_PREFIX_LENGTH+1, runtime.length - KRUNTIME_PREFIX_LENGTH-1]
-        puts "... Examining DEVICE: #{device["name"]} (#{runtime})"
+        DebugUtils.debug_line "... Examining DEVICE: #{device["name"]} (#{runtime})"
+        DebugUtils.debug_line "... DEVICE Folder: #{device_folder}"
 
         appState_plist = device_folder + KNEW_SIMULATORS_APPLICATIONSTATE_PLIST
         if File.exist?(appState_plist) then
+            # DebugUtils.debug_line appState_plist
             # any apps?
             plist = CFPropertyList::List.new(:file => appState_plist)
             appState = CFPropertyList.native_types(plist.value)
             myApps = appState.select { |k,v| not k.start_with? KCOM_APPLE_APPS_PREFIX  }
 
-            puts "MY APPS:"
+            DebugUtils.debug_line "MY APPS:"
             myApps.each { |k,v| 
-              puts "    BundleId: #{k}"
-              puts "    SandBox Path: #{v['compatibilityInfo']['sandboxPath']}"
-              puts "  ----"
+              DebugUtils.debug_line "      BundleId: #{k}"
+              DebugUtils.debug_line "      SandBox Path: #{v['compatibilityInfo']['sandboxPath']}" if (v['compatibilityInfo'])
+              DebugUtils.debug_line "  ----"
             }
         else 
-            puts "NO APPS!"
+            DebugUtils.debug_line "NO APPS!"
         end
 
     end
